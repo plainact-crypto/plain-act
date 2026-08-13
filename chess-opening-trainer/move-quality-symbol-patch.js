3004cd20221d532c78ba1a3286f6b7bd429a63c9
plainact-crypto <plainact@gmail.com>
2026-08-13 15:57:54 +0300

    Restore move quality patch entrypoint

---
+++ chess-opening-trainer/move-quality-symbol-patch.js
@@ -0,0 +1,4 @@
+// Move-quality patch entrypoint temporarily disabled pending conservative classifier replacement.
+globalThis.__COT_SYMBOL_ONLY_MOVE_QUALITY__=true;
+globalThis.__COT_GUIDED_MOVE_QUALITY_SYMBOLS__=true;
+globalThis.__COT_MOVE_QUALITY_V2_PENDING__=true;
