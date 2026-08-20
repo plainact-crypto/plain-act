e4e02cc5af65b5c790c85f8280afe892c047c93e
plainact-crypto <plainact@gmail.com>
2026-08-20 15:12:19 +0300

    Configure Astro for plain-act.com

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,7 @@
 import { defineConfig } from 'astro/config';
 
 export default defineConfig({
-  site: 'https://plainact-crypto.github.io',
-  base: '/plain-act/',
+  site: 'https://plain-act.com',
+  base: '/',
   trailingSlash: 'always'
 });
